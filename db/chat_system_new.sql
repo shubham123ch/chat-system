a720bd9ce90d4209513d1cf560cbf71c8a3ac713
shubham123ch <65480772+shubham123ch@users.noreply.github.com>
2020-06-12 21:19:26 +0530

    Add files via upload

---
+++ db/chat_system_new.sql
@@ -0,0 +1,75 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.2 
+MySQL - 5.5.5-10.1.28-MariaDB : Database - chat_system
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`chat_system` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `chat_system`;
+
+/*Table structure for table `chat` */
+
+DROP TABLE IF EXISTS `chat`;
+
+CREATE TABLE `chat` (
+  `chatid` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `chatroomid` int(11) NOT NULL,
+  `message` varchar(200) NOT NULL,
+  `chat_date` datetime NOT NULL,
+  PRIMARY KEY (`chatid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `chat_member` */
+
+DROP TABLE IF EXISTS `chat_member`;
+
+CREATE TABLE `chat_member` (
+  `chat_memberid` int(11) NOT NULL AUTO_INCREMENT,
+  `chatroomid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  PRIMARY KEY (`chat_memberid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `chatroom` */
+
+DROP TABLE IF EXISTS `chatroom`;
+
+CREATE TABLE `chatroom` (
+  `chatroomid` int(11) NOT NULL AUTO_INCREMENT,
+  `chat_name` varchar(60) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `chat_password` varchar(30) NOT NULL,
+  `userid` int(11) NOT NULL,
+  PRIMARY KEY (`chatroomid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `uname` varchar(60) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `access` int(1) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+INSERT INTO `user` (`userid`, `username`, `password`, `uname`, `photo`, `access`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', '', 1);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
